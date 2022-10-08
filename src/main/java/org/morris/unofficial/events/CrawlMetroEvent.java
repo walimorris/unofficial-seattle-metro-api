f12b54ab4cd028086dd9c76f9a21dcd0a02748a7
Wali Morris <walimmorris@gmail.com>
2022-10-08 02:11:46 -0700

    update CrawlMetroEvent.java to check for latest document scans

--- src/main/java/org/morris/unofficial/events/CrawlMetroEvent.java
+++ src/main/java/org/morris/unofficial/events/CrawlMetroEvent.java
@@ -5,16 +5,23 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.joda.time.DateTime;
 
 import java.io.InputStreamReader;
-import java.io.IOException;
 import java.io.BufferedReader;
-import java.io.PrintWriter;
 import java.io.File;
+import java.io.PrintWriter;
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Date;
+import java.util.List;
 
 public class CrawlMetroEvent {
     final String REGION = System.getenv("REGION"); // region
@@ -28,8 +35,67 @@ public class CrawlMetroEvent {
         logger.log(String.format("Seattle Metro crawl event triggered: %s", event.getId()));
 
         printMetroDumpToTmp(logger);
-        putS3File();
+
+        // Do we need to crawl the data - if no unprocessed document exists then yes
+        // If document exists, let's match up against new crawled .txt in /tmp
+
+        if (!bucketContainsDocuments()) {
+            // contains no documents, crawl immediately
+            putS3File();
+        } else {
+            if (bucketContainsDocuments()) {
+                // documents exists - crawl and match against last crawled document
+                // if they match then there's no use recording the document as it'll
+                // only be a copy
+                S3Object latestDocumentObject = getMostRecentDocument();
+
+                // compare the text from the latest object and dumped document sitting in /tmp
+            }
+        }
         return "success";
+    }
+
+    private S3Object getMostRecentDocument() {
+        AmazonS3 s3Client = getS3Client();
+        ObjectListing objectListing = s3Client.listObjects(BUCKET);
+        List<S3ObjectSummary> objectSummaries = objectListing.getObjectSummaries();
+
+        // Get the first date by pulling the first objects last modified date
+        Date currentDate = objectSummaries.get(0).getLastModified();
+        S3ObjectSummary latestObjectSummary = null;
+        for (S3ObjectSummary objectSummary : objectSummaries.subList(1, objectSummaries.size())) {
+            if (objectSummary.getLastModified().compareTo(currentDate) > 0) {
+                // it is greater - last modified date comes after first date
+                latestObjectSummary = objectSummary;
+                // update the date
+                currentDate = latestObjectSummary.getLastModified();
+            }
+        }
+        // return the object
+        S3Object latestDocumentObject = null;
+        if (latestObjectSummary != null) {
+            GetObjectRequest getObjectRequest = new GetObjectRequest(latestObjectSummary.getKey(), BUCKET);
+            latestDocumentObject = s3Client.getObject(getObjectRequest);
+        }
+        return latestDocumentObject;
+    }
+
+    private boolean bucketContainsDocuments() {
+        AmazonS3 s3Client = getS3Client();
+        Bucket unprocessedBucket = null;
+        List<Bucket> buckets = s3Client.listBuckets();
+        for (Bucket bucket : buckets) {
+            if (bucket.getName().equals(BUCKET)) {
+                unprocessedBucket = bucket;
+            }
+        }
+        // see if bucket contains object(s) (document)
+        if (unprocessedBucket != null) {
+            ObjectListing objectListing = s3Client.listObjects(unprocessedBucket.getName());
+            return objectListing.getObjectSummaries().size() > 0;
+        }
+        s3Client.shutdown();
+        return false;
     }
 
     /**
